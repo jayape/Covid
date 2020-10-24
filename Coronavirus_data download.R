9e2f79f1833ed3220a9e7765cdbe2a7186971977
John Pertell <jpertell@gmail.com>
2020-10-24 15:51:07 -0500

    Now can read/save all data from Johns Hopkins or just the previous day.

--- Coronavirus_data download.R
+++ Coronavirus_data download.R
@@ -1,12 +1,14 @@
-# script for all Covid data
+# This script gets all Covid-19 data from the John Hopkins University
+# It can store it in a SQL databasae as well as locally. 
 
 library(lubridate)
 library(dplyr)
 library(data.table)
 library(tibble)
+library(tidyr)
+library(RODBC)
 
 start_date <- as.Date('2020-01-22')
-results <- data.frame()
 data1 <- data.frame()
 data2 <- data.frame()
 data3 <- data.frame()
@@ -21,8 +23,8 @@ while (start_date <= Sys.Date() - days(1)) {
   temp <- format(start_date, '%m-%d-%Y')
   url <- capture.output(cat
                         ('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/', temp, '.csv',sep=""))
-  
   data <- read.csv(url, sep = ",")
+
   if (ncol(data) == 6) {
     names(data) <- dataNames1
     data1 <- rbind(data1, data)
@@ -36,11 +38,11 @@ while (start_date <= Sys.Date() - days(1)) {
     names(data) <- dataNames4
     data4 <- rbind(data4, data)
   }
-  
   start_date <- start_date + days(1)  
 }
 
-
+# The next 2 blocks are if I save the data to .csv files, otherwise not needed.
+# For single day file I'll change read/write to include datye in the file name
 # Save data
 write.table(data1, file = './CovidData/data1.csv')
 write.table(data2, file = './CovidData/data2.csv')
@@ -48,24 +50,40 @@ write.table(data3, file = './CovidData/data3.csv')
 write.table(data4, file = './CovidData/data4.csv')
 
 # Read saved data
-one <- read.csv('./CovidData/data1.csv', sep='')
-two <- read.csv('./CovidData/data2.csv', sep='')
-three <- read.csv('./CovidData/data3.csv', sep='')
-four <- read.csv('./CovidData/data4.csv', sep='')
+#data1 <- read.csv('./CovidData/data1.csv', sep='', stringsAsFactors = FALSE)
+#data2 <- read.csv('./CovidData/data2.csv', sep='', stringsAsFactors = FALSE)
+#data3 <- read.csv('./CovidData/data3.csv', sep='', stringsAsFactors = FALSE)
+#data4 <- read.csv('./CovidData/data4.csv', sep='', stringsAsFactors = FALSE)
+# End write/read .csv files
 
+# This block ensures that the number of columns is consistent and in the same order before combining
+data1 <- data1 %>% add_column(FIPS = NA, Admin2 = NA, Latitude = NA, Longitude = NA, Active = NA, Combined.Key = NA,  Incident.Rate = NA, Case.Fatality_Ratio = NA, Updated = NA)
+data1 <- data1[, c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Updated', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')]
+data2 <- data2 %>% add_column(FIPS = NA, Admin2 = NA, Active = NA, Combined.Key = NA,  Incident.Rate = NA, Case.Fatality_Ratio = NA, Updated = NA)
+data2 <- data2[, c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Updated', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')]
+data3 <- data3 %>% add_column(Incident.Rate = NA, Case.Fatality_Ratio = NA, Updated = NA)
+data3 <- data3[, c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Updated', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')]
+data4 <- data4 %>% add_column(Updated = NA)
+data4 <- data4[, c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Updated', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')]
 
-one <- one %>% add_column(FIPS = NA, Admin2 = NA, Latitude = NA, Longitude = NA, Active = NA, Combined.Key = NA,  Incident.Rate = NA, Case.Fatality_Ratio = NA)
-one <- one[, c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')]
-two <- two %>% add_column(FIPS = NA, Admin2 = NA, Active = NA, Combined.Key = NA,  Incident.Rate = NA, Case.Fatality_Ratio = NA)
-two <- two[, c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')]
-three <- three %>% add_column(Incident.Rate = NA, Case.Fatality_Ratio = NA)
-three <- three[, c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')]
+CovidData <- rbind(data1, data2, data3, data4)
 
+# Convert Last.Updated to a date field
+# CovidData$Updated <- strptime(CovidData$Last.Update, "%Y-%m-%d")
 
-CovidData <- rbind(one, two, three, four)
+CovidData$Updated <-as.POSIXct(CovidData$Updated)
 
-library(RODBC)
+tempNA <- filter(CovidData, is.na(Updated) == TRUE)
+tempNotNA <- filter(CovidData, is.na(Updated) == FALSE)
 
+tempNA$Updated <- as.Date(as.POSIXct(tempNA$Last.Update, format = "%m/%d/%y"))
+
+allData <- rbind(tempNA, tempNotNA)
+
+allData <- allData[,c(1:4, 6:15)]
+names(allData)[5] <- 'Last.Update'
+
+# Add to local database. Changing to Azure later
 connectionString <-  "Driver={SQL Server};
                       Server=PERTELL01,1433;
                       Database=Covid;
@@ -73,8 +91,11 @@ connectionString <-  "Driver={SQL Server};
 
 myConn <- odbcDriverConnect(connectionString)
 
-sqlSave(myConn, CovidData, "CovidData", append = TRUE, rownames = FALSE)
+sqlSave(myConn, allData, "CovidData", append = TRUE, rownames = FALSE)
 close(myConn)
+
+
+
 # Extra stuff
 
 KenoshaData <- filter(CovidData, Province.State == 'Wisconsin'& Last.Update %like% '2020-10-16')
@@ -109,3 +130,6 @@ ncol(results)
 results$Longitude <- NA
 
 url
+
+
+parse_date_time('1/22/2020 17:00', orders = c('ymd', 'dmy', 'mdy'))
---
+++ Daily_coronavirus_data_download.R
@@ -0,0 +1,48 @@
+# This script downloads the previous day's Covid-19 data from the John Hopkins site.
+# It stores the data in a SQL database and/or locally.
+
+library(lubridate)
+library(dplyr)
+library(data.table)
+library(tibble)
+library(tidyr)
+library(RODBC)
+
+CovidData <- data.frame()
+
+dataNames <- c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')
+
+start_date <- Sys.Date() - days(1)
+temp <- format(start_date, '%m-%d-%Y')
+url <- capture.output(cat
+                     ('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/', temp, '.csv',sep=""))
+  
+CovidData <- read.csv(url, sep = ",")
+names(CovidData) <- dataNames  
+
+# The next 2 blocks are if I save the data to .csv files, otherwise not needed.
+fileName <- paste('./CovidData/', start_date, '.csv', sep = '')
+write.table(CovidData, file = fileName)
+#CovidData <- read.csv(fileName, sep='', stringsAsFactors = FALSE)
+# End write/read .csv files
+
+CovidData <- CovidData %>% add_column(Updated = NA)
+CovidData <- CovidData[, c('FIPS', 'Admin2', 'Province.State', 'Country.Region', 'Last.Update', 'Updated', 'Latitude', 'Longitude', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined.Key', 'Incident.Rate', 'Case.Fatality_Ratio')]
+
+# Convert Last.Updated to a date field
+CovidData$Updated <-as.POSIXct(CovidData$Updated)
+
+allData <- CovidData[,c(1:4, 6:15)]
+names(allData)[5] <- 'Last.Update'
+
+# Add to local database. Changing to Azure later
+connectionString <-  "Driver={SQL Server};
+                      Server=PERTELL01,1433;
+                      Database=Covid;
+                      Integrated Security=SSPI;"
+
+myConn <- odbcDriverConnect(connectionString)
+
+sqlSave(myConn, allData, "CovidData", append = TRUE, rownames = FALSE)
+close(myConn)
+
